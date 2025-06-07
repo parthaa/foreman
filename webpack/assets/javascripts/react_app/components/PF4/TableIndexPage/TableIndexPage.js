01d2fb4f539f42b2033909534de027e98d3e2b55
Partha Aji <paji@redhat.com>
2025-06-06 21:44:16 -0400

    Fixes #38216 - Use restrict Search query appropriately in table index

--- webpack/assets/javascripts/react_app/components/PF4/TableIndexPage/TableIndexPage.js
+++ webpack/assets/javascripts/react_app/components/PF4/TableIndexPage/TableIndexPage.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-lines */
-import React, { useMemo } from 'react';
+import React, { useMemo, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { QuestionCircleIcon } from '@patternfly/react-icons';
 import { useHistory } from 'react-router-dom';
@@ -72,7 +72,7 @@ A page component that displays a table with data fetched from the API. It provid
 @param {string} {idColumn} - Not needed when passing children. The column name to use for RowSelectTd to pass to its selectOne function
 @param {function} {rowKebabItems} - Not needed when passing children. A function that takes a single result object and returns an array of kebab items to be displayed in the last column
 @param {function} {updateSearchQuery} - Pass in the updateSearchQuery function returned from useBulkSelect.
-@param {function} {restrictedSearchQuery} - If included, normalize the search query to add this to all search queries to restrict search results without altering the search input value. Useful for limiting results to an initial selection. 
+@param {function} {restrictedSearchQuery} - If included, normalize the search query to add this to all search queries to restrict search results without altering the search input value. Useful for limiting results to an initial selection.
 @param {boolean} {updateParamsByUrl} - If true, update pagination props from URL params. Default is true.
 @param {string} {bookmarksPosition} - The position of the bookmarks dropdown. Default is 'left', which means the menu will take up space to its right.
 */
@@ -164,6 +164,16 @@ const TableIndexPage = ({
     updateSearchQuery,
     pushToHistory: updateParamsByUrl,
   });
+
+  useEffect(() => {
+    if (restrictedSearchQuery) {
+      const queryParams = {
+        ...params,
+        search: restrictedSearchQuery(search) ?? (search || ''),
+      };
+      setParamsAndAPI(queryParams);
+    }
+  }, []);
 
   const onPagination = newPagination => {
     setParamsAndAPI({ ...params, ...newPagination });
